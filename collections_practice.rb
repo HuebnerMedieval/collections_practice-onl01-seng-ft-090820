04aec2e29bb75adcd7e585a270780651a7e03885
Eli Huebner <elimhuebner@gmail.com>
2020-09-10 17:02:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,13 +21,7 @@ end
 def sort_array_char_count (array)
   char_count_sort = array.sort do |a, b|
     #binding.pry
-    if a.length == b.length
-      0
-    elsif a < b.length
-      -1
-    elsif a.length > b.length
-      1
-    end
+    a.length <=> b.length
   end
   char_count_sort
 end
